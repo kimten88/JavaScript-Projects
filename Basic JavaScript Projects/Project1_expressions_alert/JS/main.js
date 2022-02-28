3db4489de3c12c07f7012f06098c77c18ed2a3fd
kimten88 <kimtendulkar88@gmail.com>
2022-02-28 11:02:05 -0800

    basic JavaScript projects submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("What's up?"); //having text display
+
+var A = "Feeling stringy today! " //Defining Variable A with a string value
+var result = A.fontcolor("blue") //According to W3 this method has been deprecated
+document.write(A); //having the browser display Variavble A
+
+document.write("We said\"It\'s a great pleasure\"!"); //printing a string with punctuation
+
+document.write(" With all of the time, effort, and " //printing a concatenated string
+    + "energy, we have found what we were searching for. ");
+
+var B = "concatenated" + "string " //defining variable B and assigninng concatenated string value to variable 
+document.write(B) //displaying variable to browser
+
+var Vehicles = "Honda", Sedan = "Civic", Truck = "Ridgeline", SUV = "Pilot"; //executing multiple varaibles assignment
+document.write(SUV)
+
+document.write(3 + 3); //written expression
